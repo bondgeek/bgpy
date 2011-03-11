e25bc33ee3580cf72ddcba5284ffeae166e99ffb
bart mosley <bartm@bondgeek.com>
2011-03-11 10:02:50 -0500

    documenting xldb

--- xldb.py
+++ xldb.py
@@ -13,22 +13,29 @@ def xl_to_date(xdate, _datemode = 1):
     yyyy, mm, dd, h, m, s =xlrd.xldate_as_tuple(xdate.value, _datemode)
     return date(yyyy, mm, dd)
 
-def xlValue(x, datemode=1, hash_comments=1):
+def xlValue(x, datemode=1, hash_comments=1, strip_text=1):
     try:
-        if(x.ctype == 0):
+        if(x.ctype == xlrd.XL_CELL_EMPTY or
+           x.ctype == xlrd.XL_CELL_BLANK or
+           x.ctype == xlrd.XL_CELL_ERROR):
             return ""
-        if(x.ctype == 1):
+            
+        if(x.ctype == xlrd.XL_CELL_TEXT):
             t = x.value.encode()
             if hash_comments and t[0] == '#':
                 return None
             else:
-                return t
-        if(x.ctype == 3):
+                return t.strip() if strip_text else t
+                
+        if(x.ctype == xlrd.XL_CELL_DATE):
             yyyy, mm, dd, h, m, s = xlrd.xldate_as_tuple(x.value,
                                                          datemode)
-            return date(yyyy,mm,dd)
+            return date(yyyy, mm, dd)
+            
         else:
+            # that leaves XL_CELL_NUMBER and XL_CELL_BOOLEAN
             return x.value
+            
     except:
         return None 
 
